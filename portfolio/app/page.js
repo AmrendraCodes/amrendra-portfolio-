5e7f66fe24ad4906be558ae8e05c8345eb8d4926
AmrendraCodes <amrendra1999official@gmail.com>
2026-06-07 08:37:29 +0530

    File commited

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,6 +1,5 @@
 // Home Page - Landing page with all sections
 import Hero from '../components/Hero';
-import Stats from '../components/Stats';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Testimonials from '../components/Testimonials';
@@ -11,7 +10,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Stats />
       <About />
       <Projects />
       <Testimonials />
